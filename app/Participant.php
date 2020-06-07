d01a01c730da47bb5c7dda30c10e0ba20bf99f3b
Cibulsky Dmitry <justepicman@gmail.com>
2020-06-07 21:27:10 +0300

    no message

--- app/Participant.php
+++ app/Participant.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Participant extends Model
 {
+    public $with = ['account'];
+
     public function account() {
         return $this->belongsTo(User::class, 'account_id');
     }
