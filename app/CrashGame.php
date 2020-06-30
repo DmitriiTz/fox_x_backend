b14634053aa472472677a9aa48e5879285e98cba
DmitriiTz <justepicman@gmail.com>
2020-06-30 23:12:51 +0300

    no message

--- app/CrashGame.php
+++ app/CrashGame.php
@@ -7,8 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class CrashGame extends Model
 {
     protected $table = 'crashgames';
-    protected $fillable = ['price','number','status','create_game','rand_number', 'stop_game'];
-    protected $hidden = ['profit'];
+    protected $fillable = ['price','number','status','create_game','rand_number', 'profit', 'stop_game'];
     const STATUS_NOT_STARTED = 0;
     const STATUS_PLAYING = 1;
     const STATUS_FINISHED = 2;
