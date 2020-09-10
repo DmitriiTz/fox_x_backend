b66eaf82c3fcf98027ca6377a98abfc4e5fd381c
Pavel Lapotnikov <38629071+lpvmak@users.noreply.github.com>
2020-09-10 19:28:00 +0300

    Added game init

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -28,7 +28,11 @@ class MainController extends Controller
         }
 
         $gameType = GameType::find($gameTypeId);
-        $game = HistoryGame::query()->orderBy('created_at', 'desc')->where('game_id', 3)->where('animation_at', '>', now()->toIso8601String())->where('game_type_id', $gameTypeId)->whereNotIn('status_id', [4, 0])->first();
+        $game = HistoryGame::query()->eagerLoadRelations([
+            'participants' => function ($query) {
+                $query->eagerLoadRelations('account');
+            }
+        ])->orderBy('created_at', 'desc')->where('game_id', 3)->where('animation_at', '>', now()->toIso8601String())->where('game_type_id', $gameTypeId)->whereNotIn('status_id', [4, 0])->first();
 
         $result = [];
         if (!$game) {
